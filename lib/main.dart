7700a81979db1919af32bdbe0abb7b170466a737
Gowtham <gowtham.n@nexware-global.com>
2020-10-12 19:03:11 +0530

    Alingment change and button changes

--- lib/main.dart
+++ lib/main.dart
@@ -55,76 +55,96 @@ class _LoginPageState extends State<LoginPage> {
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'ようこそ',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
-            ),
-            Text('アプリを使用するにはログインしてください。'),
-            SizedBox(
-              height: 7,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: new InputDecoration(
-                labelText: 'メールアドレス',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'ようこそ',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
               ),
-            ),
-            SizedBox(
-              height: 7,
-            ),
-            TextFormField(
-              obscureText: false,
-              decoration: new InputDecoration(
-                labelText: 'パスワード',
+              Text('アプリを使用するにはログインしてください。'),
+              SizedBox(
+                height: 7,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ResetPassword()));
-              },
-              child: Text(
-                'パスワードを忘れた方はこちら',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                autofocus: false,
+                decoration: new InputDecoration(
+                  labelText: 'メールアドレス',
                 ),
               ),
-            ),
-            SizedBox(
-              height: 18,
-            ),
-            MaterialButton(
-              color: Colors.blue,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  side: BorderSide(color: Colors.white)),
-              onPressed: () {
-                print('click me');
-              },
-              minWidth: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.symmetric(
-                vertical: 12.0,
+              SizedBox(
+                height: 7,
               ),
-              child: Text(
-                'ログイン',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
+              TextFormField(
+                autofocus: false,
+                decoration: new InputDecoration(
+                  labelText: 'パスワード',
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => ResetPassword()));
+                },
+                child: Text(
+                  'パスワードを忘れた方はこちら',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: RaisedButton(
+                  child: Text(
+                    'ログイン',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  color: Colors.blue,
+                  onPressed: () {
+                    print('click me');
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              )
+              /*MaterialButton(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        side: BorderSide(color: Colors.white)),
+                    onPressed: () {
+                      print('click me');
+                    },
+                    minWidth: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 12.0,
+                    ),
+                    child: Text(
+                      'ログイン',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),*/
+            ],
+          ),
         ),
       ),
     );
