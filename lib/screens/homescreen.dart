7872faf70e77a1c612c79919bace28f0ed7c3645
Aswin <aswin.a@nexware-global.com>
2021-02-02 09:30:46 +0530

    Home Screen Calendar bug fix

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -807,7 +807,7 @@ class _HomeScreeenState extends State<HomeScreeen> {
     } else if (month == 4 || month == 6 || month == 9 || month == 11) {
       return 31;
     } else if (month == 2) {
-      return year % 4 == 0 ? 30 : 28;
+      return year % 4 == 0 ? 30 : 29;
     }
   }
 
